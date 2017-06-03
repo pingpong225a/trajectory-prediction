09b29f118d64581f16d8f91451295c6b81dcb4cd
Mikael <mjorda@stanford.edu>
2017-06-03 16:30:57 -0700

    refactored code

--- NatNetClient.py
+++ NatNetClient.py
@@ -193,18 +193,13 @@ class NatNetClient:
             offset += 12
             trace( "\tMarker", i, ":", pos[0],",", pos[1],",", pos[2] )
         
-            if(pos[2] > 0.5 and pos[2] < 2.0 and pos[0] > -0.7 and pos[0] < 0.3): 
-                # NOTE: currently not predicting in Z direction
-                self.trajectory.record_pos([pos[2], pos[0], pos[1]], frameNumber)
-                self.redis_client.set("ball_pos", str(pos[2]) + " " + str(pos[0]) + " " + str(pos[1]))
-  
-                x_pred = self.trajectory.calc_trajectory()
-                if x_pred != None:
-                    self.redis_client.set("cs225a::robot::kuka_iiwa::tasks::ee_pos_des", str(x_pred[0]) + " " + str(x_pred[1]) + " " + str(x_pred[2]))
-                    self.redis_client.set("ball_pred_pos", str(x_pred[0]) + " " + str(x_pred[1]) + " " + str(x_pred[2]))
-            #else:
-            #        self.redis_client.set("cs225a::robot::kuka_iiwa::tasks::ee_pos_des", str(0.3) + " " + str(-0.5) + " " + str(0.5))
-        
+            self.redis_client.set("ball_pos", str(pos[2]) + " " + str(pos[0]) + " " + str(pos[1]))
+            x_pred = self.trajectory.calc_trajectory([pos[2], pos[0], pos[1]], frameNumber)
+            if x_pred != None:
+                self.redis_client.set("cs225a::robot::kuka_iiwa::tasks::ee_pos_des", str(x_pred[0]) + " " + str(x_pred[1]) + " " + str(x_pred[2]))
+                print("ball_pred_pos = ",  str(x_pred[0]) + " " + str(x_pred[1]) + " " + str(x_pred[2]))
+                self.redis_client.set("ball_pred_pos", str(x_pred[0]) + " " + str(x_pred[1]) + " " + str(x_pred[2]))
+
         # Rigid body count (4 bytes)
         rigidBodyCount = int.from_bytes( data[offset:offset+4], byteorder='little' )
         offset += 4
--- trajectory.py
+++ trajectory.py
@@ -1,156 +1,137 @@
 import numpy as np 
-#import queue 
 import redis 
-import time
+import time as pythontime
 
-playing_field = [] # (xmin,xmax) (ymin,ymax) (zmin, zmax)
-dest_x = 20 # x location at which the kuka is 
+WORKSPACE_MIN_X = 0.1 # 0.1
+WORKSPACE_MAX_X = 0.9  # 0.7
+WORKSPACE_MIN_Y = -0.75 #  -0.61
+WORKSPACE_MAX_Y =  -0.2 # -0.25
 
+STAGE_MIN_X = 0
+STAGE_MAX_X = 2.0
+STAGE_MIN_Y = -0.7
+STAGE_MAX_Y = 0.3 
+STAGE_MIN_Z = -0.05
 
-# ()
 MAX_POSITIONS = 10 # 30
 KUKA_POS_Y = 0 # in optitrack coordinates 
 KUKA_POS_Z = 0.4 # 0.6  # 0.3
 TABLE_POS_Z = 0.07 # 0.33 # 0
 
 TRACK_BALL = True
+HOME_POS = [0.05, -0.62, KUKA_POS_Z]
+
+wait_time = 0 
 
 class Trajectory: 
     def __init__(self):
         global MAX_POSITIONS
-        #self.ball_trace = queue.Queue(max_size=50) 
-        #self.redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
         self.positions = np.zeros((MAX_POSITIONS, 3))
         self.time = np.ones((MAX_POSITIONS, 2))
         self.index = 0
         self.num_points = 0
-        self.pred_hist = np.zeros((MAX_POSITIONS, 3))
-        self.pred_weight = np.exp(np.linspace(-MAX_POSITIONS,-1,MAX_POSITIONS))
-        self.pred_weight = self.pred_weight / np.sum(self.pred_weight)
 
-        # for debugging
-        self.debug_count = 0 
-
-    
     def record_pos(self, pos, time):
-        global MAX_POSITIONS
-        global TRACK_BALL
-        if pos[2] < TABLE_POS_Z + 0.10:
-            TRACK_BALL = False
-        if not TRACK_BALL:
-            return
         self.positions[self.index, :] = pos[0:3] 
         self.time[self.index, 0] = time / 200.0
         self.index = (self.index + 1) % MAX_POSITIONS
         if self.num_points < MAX_POSITIONS:
             self.num_points += 1 
 
-    def filter_ball_des(self, a):
-        x, y, z = a
-        
-        # if x > 0.5:
-        #     x = 0.5
+    def clear_history(self):
+        self.positions = np.zeros((MAX_POSITIONS, 3))
+        self.time = np.ones((MAX_POSITIONS, 2))
+        self.index = 0
+        self.num_points = 0
 
-        if y < -0.7:
-            y = -0.7
-            
-        if y > -0.2:
-            y = -0.2
-        # if z > 0.8:
-        #     z = 0.8
-        # if z < 0.3:
-        #     z = 0.3
-        return [x, y, z]
+    def going_away_from_kuka(self, mx):
+        return mx >= 0
 
-    def calc_trajectory(self):
+    def location_within_workspace(self, x, y, z):
+        global WORKSPACE_MIN_X
+        global WORKSPACE_MAX_X
+        global WORKSPACE_MIN_Y
+        global WORKSPACE_MAX_Y
+        return x > WORKSPACE_MIN_X and x < WORKSPACE_MAX_X \
+            and y > WORKSPACE_MIN_Y and y < WORKSPACE_MAX_Y
+
+    def location_on_stage(self, pos):
+        x, y, z = pos[0:3]
+        global STAGE_MIN_X
+        global STAGE_MAX_X
+        global STAGE_MIN_Y
+        global STAGE_MAX_Y
+        global STAGE_MIN_Z
+        return x > STAGE_MIN_X and x < STAGE_MAX_X \
+             and y > STAGE_MIN_Y and y < STAGE_MAX_Y \
+             and z > STAGE_MIN_Z
+
+    def ball_hit_table(self, pos):
+        global TABLE_POS_Z
+        x, y, z = pos[0:3]
+        return z < TABLE_POS_Z + 0.03
+
+    def calc_trajectory(self, ball_pos, time):
         global MAX_POSITIONS
         global TABLE_POS_Z
         global KUKA_POS_Z
-
         global TRACK_BALL
-        if not TRACK_BALL:
-            time.sleep(0.8)
-            return [0.05, -0.62, KUKA_POS_Z] # return home
+        global HOME_POS
 
-        if self.num_points >= MAX_POSITIONS:
-            tx = np.linalg.lstsq(self.time, self.positions[:, 0])
-            ty = np.linalg.lstsq(self.time, self.positions[:, 1])
-            #tz = np.linalg.lstsq(self.time, self.positions[:, 2])
-            tz = np.polyfit(self.time[0:MAX_POSITIONS,0], self.positions[0:MAX_POSITIONS, 2],2)
-            cx = tx[0][1]
-            mx = tx[0][0]
-            cy = ty[0][1]
-            my = ty[0][0]
-            # cz = tz[0][1]
-            # mz = tz[0][0]
-            pz2 = tz[0]
-            # pz2 = -1.0 * 3.8
-            pz1 = tz[1]
-            pz0 = tz[2]
-            pz0 -= TABLE_POS_Z
-            t_old = (0.1 - 1.0 * cx) / mx
-            # t = cz / mz + 0.1 
-            #roots = np.roots(tz[0:3])
-            coeffi = [pz2, pz1, pz0]
-            roots = np.roots(coeffi)
+        global wait_time 
+        curr_time = pythontime.time() 
+        if curr_time < wait_time:
+            return None    
 
-            # if pz0 > 0: 
-            #     # ignore 
-            #     return 
-            #print ("pz2 = ", pz2)
-      
-            t = np.max(np.real(roots))
-            if self.debug_count % MAX_POSITIONS == 0:
-                #print ("t new = ", (t - self.time[self.index,0]))
-                #print ("t old = ", (t_old -  self.time[self.index,0]))
-                print(self.positions[0:MAX_POSITIONS, :])
-                print(self.time[0:MAX_POSITIONS,0])
+        # Record/predict ball if on stage
+        if self.location_on_stage(ball_pos): 
+
+            if self.ball_hit_table(ball_pos):
+                wait_time = pythontime.time() + 1 
+                self.clear_history()
+                print("returning home")
+                return HOME_POS  
+
+            self.record_pos(ball_pos, time)
+
+            # Fit trajectory on ball path  
+            if self.num_points >= MAX_POSITIONS:
+                tx = np.linalg.lstsq(self.time, self.positions[:, 0])
+                ty = np.linalg.lstsq(self.time, self.positions[:, 1])
+                tz = np.polyfit(self.time[0:MAX_POSITIONS,0], self.positions[0:MAX_POSITIONS, 2],2)
+                cx = tx[0][1]
+                mx = tx[0][0]
+                cy = ty[0][1]
+                my = ty[0][0]
+                pz2 = tz[0]
+                pz1 = tz[1]
+                pz0 = tz[2]
+                pz0 -= TABLE_POS_Z
+                coeffi = [pz2, pz1, pz0]
+                roots = np.roots(coeffi) 
+                t = np.max(np.real(roots))
                 x_pred = mx * t + cx
                 y_pred = my * t + cy
                 z_pred = KUKA_POS_Z
-                print(x_pred, y_pred, z_pred)
-                x_landing = mx * (t) + cx
-                y_landing = my * (t) + cy
-                z_landing = TABLE_POS_Z
 
-               # print("predicting landing = ", [x_landing, y_landing, z_landing])
-            self.debug_count += 1
-            dt = (t - self.time[self.index,0])
-            # x_pred = mx * t + cx
-            # y_pred = my * t + cy
-            # z_pred = mz * t + cz
+                if self.going_away_from_kuka(mx) or (not self.location_within_workspace(x_pred, y_pred, z_pred)):
+                    if self.going_away_from_kuka(mx) and (not self.location_within_workspace(x_pred, y_pred, z_pred)):
+                        print("both")
+                    elif self.going_away_from_kuka(mx):
+                        print("going away from kuka")
+                    elif not self.location_within_workspace(x_pred, y_pred, z_pred):
+                        print("location outside workspace")
+                        print([x_pred, y_pred, z_pred])
 
-            x_pred = mx * t + cx
-            y_pred = my * t + cy
-            z_pred = KUKA_POS_Z
+                    print("returning home 2")
+                    self.clear_history() 
+                    return HOME_POS
+                else: 
+                    print("sending pred")
+                    print([x_pred, y_pred, z_pred])
+                    return [x_pred, y_pred, z_pred]
+       # else: 
+           # print("location outside stage")
 
-            if x_pred < 0.05 or x_pred > 1.5 or pz2 > 0:
-            #if x_pred < 0.05 or x_pred > 1.5: #or y < -0.8 or y > -0.2:
-            #if pz2 > 0:
-                #print("x_pred = ", x_pred)
-                #print("pz2  = ", pz2)
-                self.positions = np.zeros((MAX_POSITIONS, 3))
-                self.time = np.ones((MAX_POSITIONS, 2))
-                self.index = 0
-                self.num_points = 0
-                #time.sleep(0.2)
-                return None
-                #return [0.05, -0.62, KUKA_POS_Z]
-
-
-
-            #print("time needed to reach to the prediction pos")
-            #z_pred = pz2 * t * t + pz1 * t + pz0
-            #print (x_pred, y_pred, z_pred)
-            x_pred, y_pred, z_pred = self.filter_ball_des([x_pred, y_pred, z_pred])
-            self.pred_hist[self.index][0] = x_pred
-            self.pred_hist[self.index][1] = y_pred
-            self.pred_hist[self.index][2] = z_pred
-            # x_pred = np.mean( self.pred_hist[:,0] ) * 0.1 + 0.9 * x_pred
-            # y_pred = np.mean( self.pred_hist[:,1] ) * 0.1 + 0.9 * y_pred
-            # z_pred = np.mean( self.pred_hist[:,2] ) * 0.1 + 0.9 * z_pred
-             
-            return [x_pred, y_pred, z_pred] #if y_pred > -0.5 and y_pred < 0.5 else None        
-            #return [x_landing, y_landing, z_landing]
-        return None 
+        return None
 
