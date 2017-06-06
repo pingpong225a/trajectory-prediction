1905044d9ee7913079b23ef5de9b976cc1e5e7c7
Mikael <mjorda@stanford.edu>
2017-06-06 15:17:32 -0700

    update by lin

--- trajectory.py
+++ trajectory.py
@@ -2,36 +2,45 @@ import numpy as np
 import redis 
 import time as pythontime
 
-V_Z_BOUNCE_COEFFI = -1 # The percentage of remaining z-velocity
+V_Z_BOUNCE_COEFFI = -0.8 # The percentage of remaining z-velocity
+V_X_BOUNCE_COEEFI = 0.65
+V_Y_BOUNCE_COEEFI = 0.65
 
-WORKSPACE_MIN_X = -0.05 # 0.1
-WORKSPACE_MAX_X = 0.4  # 0.4
-WORKSPACE_MIN_Y = -0.7 #  -0.61
-WORKSPACE_MAX_Y =  -0.2 # -0.25
+############################################# Never change the working space variables values are updated in real kuka
+WORKSPACE_MIN_X = -0.05 
+WORKSPACE_MAX_X = 0.45  
+WORKSPACE_MIN_Y = -0.7 
+WORKSPACE_MAX_Y = -0.35
 WORKSPACE_MIN_Z = 0.35
 WORKSPACE_MAX_Z = 0.75
+######################################################################
 
 STAGE_MIN_X = 0
-STAGE_MAX_X = 2.0
+STAGE_MAX_X = 2.5
 STAGE_MIN_Y = -0.7
-STAGE_MAX_Y = 0.3 
+STAGE_MAX_Y = 0.1 
 STAGE_MIN_Z = -0.05
 
 MAX_POSITIONS = 10 # 30
 KUKA_POS_Y = 0 # in optitrack coordinates 
 KUKA_POS_Z = 0.4 # 0.6  # 0.3
-TABLE_POS_Z = 0.02 # 0.07 # 0.33 # 0
 
+
+############ Calibration
+###############################################################################
+TABLE_POS_Z = -0.00 # 0.07 # 0.33 # 0
+###################################################################################
 TRACK_BALL = True
 HOME_POS = [0.05, -0.62, KUKA_POS_Z]
-#HOME_POS = None
 
-HITTING_X_PLANE = 0.4
+###################################################################
+HITTING_X_PLANE = 0.3 ######## Make sure HITTING_X_PLANE IS IN WORKING SPACE !!!!
+#####################################################################################
 
 wait_time = 0 
 GRAVITY = 9.8
 
-WAIT_TIME_AFTER_BOUNCE = 10 # ASK BEFORE CHANGING
+WAIT_TIME_AFTER_BOUNCE = 5 # ASK BEFORE CHANGING
 
 def trace(s):
     print(s)
@@ -55,12 +64,10 @@ class Trajectory:
         # if time_left_to_wait >= 0 and time_left_to_wait <= WAIT_TIME_AFTER_BOUNCE - 2:
         #     print("returning home...")
         #     return HOME_POS
-         
-
-
+        
     def record_pos(self, pos, time):
         self.positions[self.index, :] = pos[0:3] 
-        self.time[self.index, 0] = time / 200.0
+        self.time[self.index, 0] = time / 200.0 
         self.index = (self.index + 1) % MAX_POSITIONS
         if self.num_points < MAX_POSITIONS:
             self.num_points += 1 
@@ -79,8 +86,7 @@ class Trajectory:
         global WORKSPACE_MAX_X
         global WORKSPACE_MIN_Y
         global WORKSPACE_MAX_Y
-        return x > WORKSPACE_MIN_X and x < WORKSPACE_MAX_X \
-            and y > WORKSPACE_MIN_Y and y < WORKSPACE_MAX_Y
+        return y > WORKSPACE_MIN_Y and y < WORKSPACE_MAX_Y and z > WORKSPACE_MIN_Z and z < WORKSPACE_MAX_Z
 
     def location_on_stage(self, pos):
         x, y, z = pos[0:3]
@@ -117,31 +123,20 @@ class Trajectory:
 
         global wait_time 
         curr_time = pythontime.time() 
+
         if curr_time < wait_time:
             return None    
 
-        if abs(curr_time - wait_time) <= 0.05:
-            print("resuming data collection")
-
-
-        # time_left_to_wait =  wait_time - curr_time 
-        # if time_left_to_wait >= 0 and time_left_to_wait <= WAIT_TIME_AFTER_BOUNCE - 2:
-        #     print("returning home...")
-        #     return HOME_POS
-
-
-
         # Record/predict ball if on stage
         if self.location_on_stage(ball_pos): 
-
             if self.ball_hit_table(ball_pos):
-                wait_time = pythontime.time() + WAIT_TIME_AFTER_BOUNCE
-                self.clear_history()
-                trace("hit table. waiting 1s")
-               # trace(ball_pos)
-               # trace("returning home")
-                #return HOME_POS
-                return None   
+               wait_time = pythontime.time() + WAIT_TIME_AFTER_BOUNCE
+               self.clear_history()
+               print("hit table. waiting %f" % WAIT_TIME_AFTER_BOUNCE)
+               return None   
+
+            if curr_time > wait_time + 0.001: 
+               print("Start to record new data")
 
             self.record_pos(ball_pos, time)
 
@@ -161,52 +156,27 @@ class Trajectory:
                 coeffi = [pz2, pz1, pz0]
                 roots = np.roots(coeffi) 
                 t_landing = np.max(np.real(roots))
-                #x_pred = mx * t + cx  
-                #y_pred = my * t + cy
-                #z_pred = KUKA_POS_Z
+                x_landing = cx + mx * t_landing
+                y_landing = cy + my * t_landing
                 
                 v_z = (2 * pz2 * t_landing + pz1 ) * V_Z_BOUNCE_COEFFI
                 
-                t_hitting = (HITTING_X_PLANE - cx) / mx
+                dt = (HITTING_X_PLANE - x_landing) / (mx * V_X_BOUNCE_COEEFI)
+ 
                 x_pred = HITTING_X_PLANE
-                y_pred = my * t_hitting + cy
-                dt = t_hitting - t_landing
-               # print('dt =',dt,'v_z =',v_z)
+                y_pred = (my * V_Y_BOUNCE_COEEFI) * dt + y_landing
+                
                 z_pred = TABLE_POS_Z + v_z * dt - 0.5 * GRAVITY * dt * dt
 
-
-                #z_pred = self.filter_z(z_pred, WORKSPACE_MIN_Z, WORKSPACE_MAX_Z)
-
+                print("%f %f %f"  % (x_pred, y_pred, z_pred))
                 if not self.location_within_workspace(x_pred, y_pred, z_pred):
+                    print("Not within workspace")
                     return None 
 
-
-
-       #          if self.going_away_from_kuka(mx) or (not self.location_within_workspace(x_pred, y_pred, z_pred)):
-                   
-       #             if self.going_away_from_kuka(mx) and (not self.location_within_workspace(x_pred, y_pred, z_pred)):
-       #                 trace("both")
-       #             elif self.going_away_from_kuka(mx):
-       #                 trace("going away from kuka")
-       #             elif not self.location_within_workspace(x_pred, y_pred, z_pred):
-       #                 trace("location outside workspace")
-       #                 trace([x_pred, y_pred, z_pred])
-
-       #             trace("returning home 2")
-       #             self.clear_history() 
-       #             return HOME_POS
-                
-       #          else: 
-       #             trace("sending pred")
-       #             trace([x_pred, y_pred, z_pred])
-       #             return [x_pred, y_pred, z_pred]
-       # else: 
-       #       trace("location outside stage")
- 
-                trace([x_pred, y_pred, z_pred])
-
                 return [x_pred, y_pred, z_pred]
-
-
-        return None
+            else:
+                return None
+        else:
+            self.clear_history()
+            return HOME_POS
 
