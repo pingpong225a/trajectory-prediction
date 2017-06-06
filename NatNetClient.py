215d0c7fb2dfe137e9f35498256db666f872481f
Mikael <mjorda@stanford.edu>
2017-06-06 16:57:58 -0700

    it is workinggit add NatNetClient.py trajectory.py!

--- NatNetClient.py
+++ NatNetClient.py
@@ -373,7 +373,7 @@ class NatNetClient:
             curr_time = pythontime.time() 
             time_left_to_wait =  wait_time - curr_time 
             if time_left_to_wait >= 0 and time_left_to_wait <= 2.5:
-                print("returning home....")
+                #print("returning home....")
                 self.redis_client.set("cs225a::robot::kuka_iiwa::tasks::ee_pos_des", str(home_position[0]) + " " + str(home_position[1]) + " " + str(home_position[2]))
             #if time_left_to_wait >= 0:
             #    self.redis_client.set("cs225a::robot::kuka_iiwa::tasks::ee_pos_des", str(home_position[0]) + " " + str(home_position[1]) + " " + str(home_position[2]))
